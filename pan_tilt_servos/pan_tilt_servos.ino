dda02e413f4b394f23cba461f1dae1f381c29a37
joy <joywang0222@gmail.com>
2024-07-24 11:58:11 -0700

    Create pan_tilt_servos.ino

---
+++ pan_tilt_servos/pan_tilt_servos.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+#include <Wire.h>
+
+Servo pan, tilt;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pan.attach(10);
+  tilt.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int panAngle = 0; panAngle <=180; panAngle++){
+    pan.write(panAngle);
+    Serial.println(panAngle);
+    delay(50);
+  }
+  for(int tiltAngle = 90; tiltAngle <=180; tiltAngle++){
+    tilt.write(tiltAngle);
+    Serial.println(tiltAngle);
+    delay(50);
+   }
+  
+
+}
