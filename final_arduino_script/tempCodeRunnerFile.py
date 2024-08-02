82a5518b0daba49d1a5e02d0010d75ce3795f4e3
nlee1126 <160913736+nlee1126@users.noreply.github.com>
2024-08-02 13:42:20 -0700

    Final Push

--- final_arduino_script/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-def main():
-    currentDir = '/Users/joy/Desktop/face_detection/faces'  # Change directory
-    fr = FaceRecognition(currentDir)
-
-    video_capture = None
-
-    serial_thread = threading.Thread(target=read_serial_data, args=(arduino,))
-    serial_thread.start()
